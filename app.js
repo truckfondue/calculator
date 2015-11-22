4c742e55f3f23e8b132176d34f91fd0fcd9b0f5b
Alex Moore <truckfondue@gmail.com>
2015-11-22 06:50:46 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+require('./login');
+require('./utils');
+
+document.write('Welcome to Webpack');
+document.write('Welcome to Starbucks');
+
+document.write('<br>AWESOME sauce!');
+
+
+console.log('App loaded');
