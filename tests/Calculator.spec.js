1272c0ae521c5e1b1f08fa7545c155524688c2ac
Alex Moore <truckfondue@gmail.com>
2016-01-04 20:14:02 -0700

    test framework setup

--- tests/Calculator.spec.js
+++ tests/Calculator.spec.js
@@ -2,10 +2,10 @@ require('../src/es6/Calculator');
 
 describe('Calculator', () => {
   it('should be available as an export', () => {
-    // console.log(calculator)
+    console.log(calculator)
     expect(typeof calculator).toBe('object');
   })
-  it('should be zero upon load', () => {
-    expect(calculator.answer.value).toBe('undefined');
-  });
+  // it('should be zero upon load', () => {
+  //   expect(calculator.answer.value).toBe('undefined');
+  // });
 });
