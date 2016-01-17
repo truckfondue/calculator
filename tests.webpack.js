9692034c4982da7816e7b406fe1b459031fc75df
Alex Moore <truckfondue@gmail.com>
2016-01-16 21:50:07 -0700

    added css styling

--- tests.webpack.js
+++
@@ -1,5 +0,0 @@
-var testsContext = require.context('./tests', true, /.spec\.js$/);
-testsContext.keys().forEach(testsContext);
-
-var srcContext = require.context('./src', true, /\.js$/);
-srcContext.keys().forEach(srcContext);
